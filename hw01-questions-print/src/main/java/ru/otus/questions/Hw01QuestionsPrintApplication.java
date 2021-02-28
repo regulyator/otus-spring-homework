5d30a2a0ca72f35926229cf6e7712d85d744de32
regulyator <regulyator777@gmail.com>
2021-03-01 00:08:20 +1000

    init

---
+++ hw01-questions-print/src/main/java/ru/otus/questions/Hw01QuestionsPrintApplication.java
@@ -0,0 +1,14 @@
+package ru.otus.questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw01QuestionsPrintApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Hw01QuestionsPrintApplication.class, args);
+	}
+
+}
