2073cb34bc8179fc35f0894dbf3dc3c6de24625d
regulyator <8vWvNj@%B%pRG&GJ@eKR>
2021-04-16 02:18:42 +1000

    WIP

--- hw08-spring-mongodb/src/main/java/ru/otus/library/domain/Book.java
+++ hw08-spring-mongodb/src/main/java/ru/otus/library/domain/Book.java
@@ -4,14 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.BatchSize;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -31,11 +28,7 @@ public class Book {
     @Field("genre")
     private Genre genre;
 
-    @Fetch(FetchMode.SELECT)
-    @BatchSize(size = 5)
-    @ManyToMany(targetEntity = Author.class, fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
-    @JoinTable(name = "book_authors",
-            joinColumns = @JoinColumn(name = "id_book"),
-            inverseJoinColumns = @JoinColumn(name = "id_author"))
+    @DBRef(lazy = true)
     private Set<Author> authors = new HashSet<>();
+
 }
