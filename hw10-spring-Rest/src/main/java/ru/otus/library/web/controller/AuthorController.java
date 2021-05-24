2b185692124c7a6f74eb7c6817a2224c0d59489f
regulyator <8vWvNj@%B%pRG&GJ@eKR>
2021-05-25 01:56:56 +1000

    wip HW10

--- hw10-spring-Rest/src/main/java/ru/otus/library/web/controller/AuthorController.java
+++ hw10-spring-Rest/src/main/java/ru/otus/library/web/controller/AuthorController.java
@@ -1,16 +1,12 @@
 package ru.otus.library.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.otus.library.domain.Author;
-import ru.otus.library.exception.ReferenceEntityException;
 import ru.otus.library.service.data.AuthorService;
 
 import java.util.Collection;
-import java.util.List;
 
 @RestController
 @RequestMapping("/library/api")
@@ -23,29 +19,29 @@ public class AuthorController {
         this.authorService = authorService;
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/authors")
-    public Collection<Author> getAllAuthors() {
-        return authorService.getAll();
+    public ResponseEntity<Collection<Author>> getAllAuthors() {
+        return ResponseEntity.ok(authorService.getAll());
     }
 
-    /*@GetMapping("/authors/{authorId}/delete")
-    public String deleteAuthor(@PathVariable String authorId) {
+    @PutMapping("/authors")
+    public ResponseEntity<Author> createAuthor(@RequestParam String newAuthorFio) {
+        return ResponseEntity.ok(authorService.create(newAuthorFio));
+    }
+
+    @DeleteMapping("/authors")
+    public ResponseEntity<?> deleteAuthor(@RequestParam String authorId) {
         authorService.removeById(authorId);
-        return REDIRECT_AUTHORS;
+        return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/authors")
+    /*@PostMapping("/authors")
     public String updateAuthor(@ModelAttribute Author author) {
         authorService.changeAuthorFio(author.getId(), author.getFio());
         return REDIRECT_AUTHORS;
     }
 
-    @PutMapping("/authors")
-    public String createAuthor(@RequestParam String newAuthorFio) {
-        authorService.create(newAuthorFio);
-        return REDIRECT_AUTHORS;
-    }
+
 
     @ExceptionHandler({ReferenceEntityException.class})
     public String referenceDeleteErrorHandler(Model model) {
