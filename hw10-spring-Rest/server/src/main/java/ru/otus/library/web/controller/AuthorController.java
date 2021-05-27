dca390f93fb604c2f92de9198a0be21924951a75
regulyator <8vWvNj@%B%pRG&GJ@eKR>
2021-05-28 01:15:58 +1000

    wip HW10

---
+++ hw10-spring-Rest/server/src/main/java/ru/otus/library/web/controller/AuthorController.java
@@ -0,0 +1,49 @@
+package ru.otus.library.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.otus.library.domain.Author;
+import ru.otus.library.service.data.AuthorService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/library/api")
+public class AuthorController {
+    private static final String REDIRECT_AUTHORS = "redirect:/authors";
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/authors")
+    public ResponseEntity<Collection<Author>> getAllAuthors() {
+        return ResponseEntity.ok(authorService.getAll());
+    }
+
+    @PutMapping("/authors")
+    public ResponseEntity<Author> createAuthor(@RequestParam String newAuthorFio) {
+        return ResponseEntity.ok(authorService.createOrUpdate(newAuthorFio));
+    }
+
+    @PostMapping("/authors")
+    public ResponseEntity<Author> updateAuthor(@RequestBody Author author) {
+        return ResponseEntity.ok(authorService.changeAuthorFio(author.getId(), author.getFio()));
+    }
+
+    @DeleteMapping("/authors")
+    public ResponseEntity<?> deleteAuthor(@RequestParam String authorId) {
+        authorService.removeById(authorId);
+        return ResponseEntity.ok().build();
+    }
+
+    /*
+    @ExceptionHandler({ReferenceEntityException.class})
+    public String referenceDeleteErrorHandler(Model model) {
+        model.addAttribute("entity", "Author");
+        return "Reference error";
+    }*/
+}
