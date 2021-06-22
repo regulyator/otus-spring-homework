b7d2aad1ce4fd9053327be2c320aed6c53da07ce
regulyator <8vWvNj@%B%pRG&GJ@eKR>
2021-06-23 01:18:01 +1000

    wip HW12

--- hw12-spring-security-simple-auth/server/src/main/java/ru/otus/library/configuration/SecurityConfiguration.java
+++ hw12-spring-security-simple-auth/server/src/main/java/ru/otus/library/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package ru.otus.library.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,10 +45,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/library/api/**").permitAll()
                 .anyRequest().fullyAuthenticated()
-                .and().httpBasic().and()
-                .logout().logoutUrl("/logout");
-        ;
+                .and()
+                .httpBasic();
     }
 }
