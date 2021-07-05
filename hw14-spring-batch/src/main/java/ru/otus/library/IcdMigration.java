2e92e141d332ac9f764ef0c4315c5d092fc92d07
regulyator <8vWvNj@%B%pRG&GJ@eKR>
2021-07-06 02:09:21 +1000

    init HW14

---
+++ hw14-spring-batch/src/main/java/ru/otus/library/IcdMigration.java
@@ -0,0 +1,12 @@
+package ru.otus.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcdMigration.java {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcdMigration.java.class, args);
+    }
+}
