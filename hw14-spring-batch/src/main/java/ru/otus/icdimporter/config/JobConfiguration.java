6a8f1c9e97c09e31bf3d4b751029ed3c11e566dc
regulyator <8vWvNj@%B%pRG&GJ@eKR>
2021-07-08 03:28:59 +1000

    wip HW14

--- hw14-spring-batch/src/main/java/ru/otus/icdimporter/config/JobConfiguration.java
+++ hw14-spring-batch/src/main/java/ru/otus/icdimporter/config/JobConfiguration.java
@@ -5,19 +5,23 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemStreamReader;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import ru.otus.icdimporter.model.IcdEntry;
-import ru.otus.icdimporter.tasklets.IcdWriter;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class JobConfiguration {
@@ -36,38 +40,48 @@ public class JobConfiguration {
                 .build();
     }
 
+
     @Bean
-    @StepScope
-    public ItemProcessor<IcdEntry, String> icdEntryProcessor() {
-        return item -> item.toString();
+    public JdbcBatchItemWriter<IcdEntry> icdEntryJdbcBatchItemWriter(DataSource dataSource,
+                                                                     NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        return new JdbcBatchItemWriterBuilder<IcdEntry>()
+                .dataSource(dataSource)
+                .namedParametersJdbcTemplate(namedParameterJdbcTemplate)
+                .sql("INSERT into icd(id, rec_code, mkb_name, mkb_code, actual, id_parent) " +
+                        "values(:id, :recCode, :mbkName, :mkbCode, :actual, :idParent)")
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .build();
     }
 
-
     @Bean
-    @StepScope
-    public ItemWriter<String> icdEntryItemWriter() {
-        return items -> System.out.println(items);
+    public ItemStreamReader<IcdEntry> icdEntryJdbcItemReader(DataSource dataSource) {
+
+        return new JdbcCursorItemReaderBuilder<IcdEntry>()
+                .dataSource(dataSource)
+                .name("icdEntryJdbcItemReader")
+                .sql("SELECT id, rec_code as recCode, mkb_name as mbkName, mkb_code as mkbCode, actual as actual, id_parent as idParent " +
+                        "from icd order by id ACS")
+                .rowMapper(new BeanPropertyRowMapper<>(IcdEntry.class))
+                .build();
     }
 
     @Bean
-    protected Step step1(StepBuilderFactory stepBuilderFactory,
-                         ItemStreamReader<IcdEntry> icdEntryItemReader,
-                         ItemProcessor<IcdEntry, String> icdEntryStringItemProcessor,
-                         ItemWriter<String> stringItemWriter) {
+    protected Step writeToTemporaryInMemoryDatabaseStep(StepBuilderFactory stepBuilderFactory,
+                                                        ItemStreamReader<IcdEntry> icdEntryItemReader,
+                                                        JdbcBatchItemWriter<IcdEntry> icdEntryJdbcBatchItemWriter) {
         return stepBuilderFactory
-                .get("step1")
-                .<IcdEntry, String>chunk(5)
+                .get("writeToTemporaryInMemoryDatabase")
+                .<IcdEntry, IcdEntry>chunk(100)
                 .reader(icdEntryItemReader)
-                .processor(icdEntryStringItemProcessor)
-                .writer(stringItemWriter)
+                .writer(icdEntryJdbcBatchItemWriter)
                 .build();
     }
 
     @Bean(name = "firstBatchJob")
     public Job job(JobBuilderFactory jobBuilderFactory,
-                   @Qualifier("step1") Step step1) {
-        return jobBuilderFactory.get("firstBatchJob")
-                .start(step1)
+                   @Qualifier("writeToTemporaryInMemoryDatabaseStep") Step writeToTemporaryInMemoryDatabaseStep) {
+        return jobBuilderFactory.get("importIcdJob")
+                .start(writeToTemporaryInMemoryDatabaseStep)
                 .build();
     }
 }
